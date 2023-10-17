00762fc0f0c9c1bff22fe57d9870c118b5b184bf
Aditya Ranjan <aditya74810@gmail.com>
2023-10-17 21:51:07 +0530

    Change in icon

--- client/src/components/HeaderBar.jsx
+++ client/src/components/HeaderBar.jsx
@@ -18,7 +18,7 @@ const Heading = styled(Typography)`
 
 
 const HeaderBar = ({ open, handleDrawer }) => {
-  const logo = 'https://seeklogo.com/images/G/google-keep-logo-0BC92EBBBD-seeklogo.com.png';
+  const logo = '  ';
   
   return (
     <Header open={open}>
